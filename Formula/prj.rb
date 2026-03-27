0717e6089c8c2df11d8ede414899d18ab8306da0
mattmc3 <mattmc3@gmail.com>
2026-03-27 16:25:20 -0400

    Use head

--- Formula/prj.rb
+++ Formula/prj.rb
@@ -1,8 +1,7 @@
 class Prj < Formula
   desc "Project jumper - quickly navigate to your Git projects"
   homepage "https://github.com/mattmc3/prj"
-  url "https://github.com/mattmc3/prj/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "21ff6d9741215dcebdf24ffe38398ae0d0528cec716f3173fd4c4da66705db41"
+  head "https://github.com/mattmc3/prj.git", branch: "main"
   license "MIT"
 
   depends_on "fd"
