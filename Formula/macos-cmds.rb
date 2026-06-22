53982e1992c52a9a0f7b2bbeb801fffffc779719
mattmc3 <mattmc3@gmail.com>
2026-06-22 15:12:15 -0400

    Add macos cmds

---
+++ Formula/macos-cmds.rb
@@ -0,0 +1,15 @@
+class MacosUtils < Formula
+  desc "macOS utility scripts"
+  homepage "https://github.com/mattmc3/macos-cmds"
+  license "MIT"
+
+  head "https://github.com/mattmc3/macos-cmds.git", branch: "main"
+
+  def install
+    bin.install Dir["bin/*"]
+  end
+
+  test do
+    assert_predicate bin/"flushdns", :exist?
+  end
+end
